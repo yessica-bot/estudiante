6666665844410065d33ff21d065e6cc53fe2f265
yessica <yess.silva@duocuc.cl>
2026-09-05 23:37:30 -0400

    JAVA-006 - Implementar Main y crear Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,15 +1,19 @@
-public class estudiante {
+public class Estudiante {
 
     private String nombre;
     private String carrera;
     private int edad;
 
-    public estudiante(String nombre, String carrera, int edad) {
+    public Estudiante(String nombre, String carrera, int edad) {
         this.nombre = nombre;
         this.carrera = carrera;
         this.edad = edad;
     }
-
+    public void mostrarInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Edad: " + edad);
+    }
     public String getNombre() {
         return nombre;
     }
@@ -34,9 +38,5 @@ public class estudiante {
         this.edad = edad;
     }
 
-    public void mostrarInformacion(){
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Carrera: " + carrera);
-        System.out.println("Edad: " + edad);
-    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         // Crear la instancia de Estudiante correctamente
-        estudiante estudiante = new estudiante(
-                "Yessica",
-                "Ingeniería en Informática",
-                25
-        );
+        Estudiante estudiante = new Estudiante(
+        "Yessica","Ingeniería en Informática", 25);;
 
         // Usar la instancia
         estudiante.mostrarInformacion();
